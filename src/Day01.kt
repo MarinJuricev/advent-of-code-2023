cb77d6bf7051fbcffe8203e4e92b17f8f0cf0559
marinjuricev <mjuric00@fesb.hr>
2023-12-05 21:40:44 +0100

    Solve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,51 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    check(part1(readInput("Day01_test")) == 142)
+    part1(readInput("Day01")).println()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    check(part2(readInput("Day02_test")) == 281)
+    part2(readInput("Day02")).println()
 }
+
+fun part1(input: List<String>): Int = input
+    .mapNotNull(::extractFirstAndLastNumberFromLine)
+    .reduce { acc, i -> acc + i }
+
+fun part2(input: List<String>) = input
+    .map(::convertTextToNumbers)
+    .mapNotNull(::extractFirstAndLastNumberFromLine)
+    .reduce { acc, i -> acc + i }
+
+private fun extractFirstAndLastNumberFromLine(
+    line: String
+) = "${line.find { it.isDigit() }}${line.findLast { it.isDigit() }}".toIntOrNull()
+
+private fun convertTextToNumbers(
+    line: String
+) = buildString {
+    line.forEachIndexed { lineIndex, c ->
+        if (c.isDigit()) append(c)
+
+        stringToIntConverter.keys.forEach { numberInString ->
+            var temp = ""
+            numberInString.forEachIndexed { index, numberValue ->
+                if (line.getOrNull(lineIndex + index) == numberValue) {
+                    temp += line.getOrNull(lineIndex + index)
+                }
+            }
+            if(temp == numberInString)
+                append(stringToIntConverter[numberInString])
+        }
+    }
+}
+
+val stringToIntConverter = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+)
