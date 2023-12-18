ec2394f3819c204da4deb5f7e31a49b3ce4747ea
marinjuricev <mjuric00@fesb.hr>
2023-12-18 23:50:52 +0100

    Solve part two of day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,9 @@
 fun main() {
     check(part1(readInput("Day04a_test")) == 13)
     part1(readInput("Day04a")).println()
+
+    check(part2(readInput("Day04a_test")) == 30)
+    part2(readInput("Day04a")).println()
 }
 
 private fun part1(
@@ -26,10 +29,49 @@ private fun part1(
         it.point
     }
 
+private fun part2(
+    input: List<String>,
+): Int {
+    val scratchCardsWithRules = input.map(::scratchCardWithRules)
+
+    return buildMap<Int, Int> {
+        scratchCardsWithRules.forEach { this[it.id] = 1 }
+        scratchCardsWithRules.forEach { card ->
+            card.idOfWonScratchCards.forEach { wonCardId ->
+                this[wonCardId] = getOrDefault(wonCardId, 0) + this[card.id]!!
+            }
+        }
+    }.values.sum()
+}
+
+
+private fun scratchCardWithRules(line: String): ScratchCardWithRules {
+    val cardIndex = cardIndexRegex.find(line)?.groupValues?.get(1)?.toInt()
+        ?: error("Can't parse cardIndex on line: $line")
+    var scratchCardIndex = cardIndex.inc()
+    val (winningNumbers, drawnNumbers) = line
+        .substringAfter(": ")
+        .split("|")
+        .map { it.trim().split(" ").filter(String::isNotBlank) }
+
+    return ScratchCardWithRules(
+        id = cardIndex,
+        idOfWonScratchCards = drawnNumbers.mapNotNull { drawnNumber ->
+            if (drawnNumber in winningNumbers) {
+                scratchCardIndex.also { scratchCardIndex++ }
+            } else null
+        }
+    )
+}
 
 data class ScratchCard(
     val id: Int,
     val point: Int,
+)
+
+data class ScratchCardWithRules(
+    val id: Int,
+    val idOfWonScratchCards: List<Int> = emptyList()
 )
 
 private val cardIndexRegex = Regex("Card\\s*(\\d+)")
