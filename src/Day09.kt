87db128314a420dced10a824687b228bbc7a1a1c
marinjuricev <mjuric00@fesb.hr>
2023-12-30 17:07:13 +0100

    Take a more FP approach

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,6 @@
 fun main() {
     check(part1(readInput("Day09_test")) == 114)
-//    part1(readInput("Day09")).println()
+    part1(readInput("Day09")).println()
 }
 
 private fun part1(
@@ -9,24 +9,9 @@ private fun part1(
     .map(String::extractInts)
     .sumOf(List<Int>::toHistoryDiagram)
 
-private fun List<Int>.toHistoryDiagram(): Int {
-    val historyDiagrams: MutableList<List<Int>> = mutableListOf(this)
-
-    while (true) {
-        val temp = mutableListOf<Int>()
-        val diagramToLookup = historyDiagrams.last()
-
-        diagramToLookup.forEachIndexed { index, i ->
-            if (index < diagramToLookup.lastIndex)
-                temp.add(diagramToLookup[index + 1] - i)
-        }
-
-        historyDiagrams.add(temp)
-        if (temp.all { it == 0 }) break
-    }
-
-
-    return historyDiagrams.fold(0) { acc, new ->
-        acc + new.last()
-    }
+private fun List<Int>.toHistoryDiagram(): Int = generateSequence(this) { lastDiagram ->
+    val newDiagram = lastDiagram.windowed(2) { it[1] - it[0] }
+    if (newDiagram.all { it == 0 }) null else newDiagram
+}.fold(0) { acc, new ->
+    acc + new.last()
 }
