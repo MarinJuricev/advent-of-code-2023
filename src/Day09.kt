3095812a88b1ce5935d5d4f8773eb4e68158e1d7
marinjuricev <mjuric00@fesb.hr>
2023-12-30 17:29:23 +0100

    Solve day 09

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,17 +1,43 @@
 fun main() {
-    check(part1(readInput("Day09_test")) == 114)
-    part1(readInput("Day09")).println()
+    check(part1(readInput("Day09_test")) { acc, new -> acc + new.last() } == 114)
+    part1(readInput("Day09")) { acc, new -> acc + new.last() }.println()
+
+    check(part2(readInput("Day09_test")) { acc, new -> new.first() - acc } == 2)
+    part2(readInput("Day09")) { acc, new -> new.first() - acc }.println()
 }
 
 private fun part1(
-    input: List<String>
+    input: List<String>,
+    action: (Int, List<Int>) -> Int,
 ): Int = input
     .map(String::extractInts)
-    .sumOf(List<Int>::toHistoryDiagram)
+    .sumOf { it.toHistoryDiagram(action) }
 
-private fun List<Int>.toHistoryDiagram(): Int = generateSequence(this) { lastDiagram ->
-    val newDiagram = lastDiagram.windowed(2) { it[1] - it[0] }
-    if (newDiagram.all { it == 0 }) null else newDiagram
-}.fold(0) { acc, new ->
-    acc + new.last()
+private fun part2(
+    input: List<String>,
+    action: (Int, List<Int>) -> Int,
+): Int = input
+    .map(String::extractInts)
+    .sumOf { it.toHistoryDiagramPart2(action) }
+
+private fun List<Int>.toHistoryDiagram(
+    action: (Int, List<Int>) -> Int,
+): Int {
+    val seq = generateSequence(this) { lastDiagram ->
+        if (lastDiagram.all { it == 0 }) return@generateSequence null
+        lastDiagram.windowed(2) { it[1] - it[0] }
+    }
+
+    return seq.fold(0, action)
+}
+
+private fun List<Int>.toHistoryDiagramPart2(
+    action: (Int, List<Int>) -> Int,
+): Int {
+    val seq = generateSequence(this) { lastDiagram ->
+        if (lastDiagram.all { it == 0 }) return@generateSequence null
+        lastDiagram.windowed(2) { it[1] - it[0] }
+    }.toList().reversed()
+
+    return seq.fold(0, action)
 }
